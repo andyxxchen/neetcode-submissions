1c01c541813184aa755d78ef19cc1735cbd9007e
Xiaxuan Chen <69415007+andyxxchen@users.noreply.github.com>
2026-05-29 16:04:43 -0700

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,101 @@
+# 对于 Course Schedule：
+
+# * DFS = 检测回边（back edge）
+# * BFS = 拓扑排序（Kahn Algorithm），不断把入度为 0 的 node 加入队列，然后每次访问队列，去消耗这个入度为 0 的
+# [course, pre] 永远翻译成 pre -> course
+
+# [[0,1]]
+'''
+    pre, -> next course
+    0 -> 1
+    adj = {
+        0: []
+        1: [3]
+        2: [3]
+        3: []
+        4: [1,2]
+    }
+
+    adj = {
+        1: [0]
+        0: [1]
+    }
+    indegree = [1, 0]
+'''
+
+from collections import deque, defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # 1. convert into a adj list
+        #    pre: [courses]
+        # 2. do dfs on each course list
+        # 3. if we met a already visited 
+        adj = defaultdict(list)
+
+        for course, pre in prerequisites:
+            adj[pre].append(course)
+        
+        visited = set()
+        visiting = set()
+
+        def dfs(course):
+            if course in visited:
+                return True
+            
+            if course in visiting: # loop
+                return False
+            
+            if len(adj[course]) == 0:
+                visited.add(course) # valid
+                return True
+
+            visiting.add(course) # 2
+            for c in adj[course]:
+                if not dfs(c): # if true means the course is reachable
+                   return False
+
+            visiting.remove(course)
+            return True
+
+        
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        return True
+        
+        
+        
+        # cur_course -> [prerequist]
+        # 1. create a indegree list -> index(course num), slot value(num of indegree)
+        # 2. initialize a deque, push all 0 indegree index into the deque
+        # 3. while deque, look it up in prerequist list, for loop traverse list -1 to all indegree , if 0 push it into the deque
+        # 4. while end, if there are some indegree > 0, False else True
+        # adj = defaultdict(list) # adj list pre -> [list of courses]
+        # indegree = [0] * numCourses
+
+        # for course, pre in prerequisites:
+        #     adj[pre].append(course)
+        #     indegree[course] += 1
+        
+        # deq = deque()
+        # for idx, inde in enumerate(indegree):
+        #     if inde == 0:
+        #         deq.append(idx)
+
+        # cnt = 0
+        # while deq:
+        #     cur = deq.popleft()
+        #     cnt += 1
+
+        #     dependecies = adj[cur] # a list
+        #     for dep in dependecies:
+        #         indegree[dep] -= 1
+        #         if indegree[dep] == 0:
+        #             deq.append(dep)
+
+        # if cnt != numCourses:
+        #     return False
+        # return True
+
+
+                
