5123584e442ca1c77eb628d3a1ec61790d88b326
Xiaxuan Chen <69415007+andyxxchen@users.noreply.github.com>
2026-05-28 19:10:01 -0700

    Add: search-for-word-ii - submission-2

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-2.py
@@ -0,0 +1,50 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Solution:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        # build Tries
+        
+        for word in words:
+            node = self.root
+            for c in word:
+                if c not in node.children:
+                    node.children[c] = TrieNode()
+                node = node.children[c]
+            node.end = True    
+        
+        res = []
+        dirs = [(0,1), (0,-1), (1,0), (-1,0)]
+        
+        def out_of_range(i, j):
+            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+                return True
+            return False
+
+        def dfs(i, j, node, cur_word, visited):
+            if node.end:
+                res.append(cur_word)
+                node.end = False
+            
+            candidates = []
+            for dx, dy in dirs:
+                if (i+dx, j+dy) in visited or out_of_range(i+dx, j+dy):
+                    continue
+                ch = board[i+dx][j+dy]
+                if ch not in node.children:
+                    continue
+                
+                visited.add((i+dx, j+dy))
+                dfs(i+dx, j+dy, node.children[ch], cur_word + ch, visited)
+                visited.remove((i+dx, j+dy))
+            
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] in self.root.children:
+                    dfs(i, j, self.root.children[board[i][j]], board[i][j], {(i, j)})
+        return res
