e1d8262be7c654a9494b57ddfe42e77fe50c22e7
Xiaxuan Chen <69415007+andyxxchen@users.noreply.github.com>
2026-06-07 00:25:06 -0700

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,23 @@
+
+import bisect
+from collections import defaultdict
+class TimeMap:
+
+    def __init__(self):
+       # hashmap k = key v = [(timestamp, value)] # do binary search on the list by the timestamp
+       self.timemap = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.timemap[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.timemap:
+            return ""
+        li = self.timemap[key]
+        
+        # find the most right index to keep the current order
+        insert_idx = bisect.bisect_right(li, timestamp, key=lambda x: x[0], )
+
+        if insert_idx == 0:
+            return ""
+        return li[insert_idx-1][1]
